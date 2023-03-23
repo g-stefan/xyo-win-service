c1f32279ac4381da0a06835791bda5f6183c07f5
Grigore Stefan <g_stefan@yahoo.com>
2023-03-23 22:52:34 +0200

    Update

--- source/XYO/WinService/Copyright.hpp
+++ source/XYO/WinService/Copyright.hpp
@@ -12,10 +12,10 @@
 #endif
 
 namespace XYO::WinService::Copyright {
-	XYO_WINSERVICE_EXPORT std::string copyright();
-	XYO_WINSERVICE_EXPORT std::string publisher();
-	XYO_WINSERVICE_EXPORT std::string company();
-	XYO_WINSERVICE_EXPORT std::string contact();
+	XYO_WINSERVICE_EXPORT const char *copyright();
+	XYO_WINSERVICE_EXPORT const char *publisher();
+	XYO_WINSERVICE_EXPORT const char *company();
+	XYO_WINSERVICE_EXPORT const char *contact();
 };
 
 #endif
